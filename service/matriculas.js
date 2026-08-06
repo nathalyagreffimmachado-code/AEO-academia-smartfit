9cd854e1b46630418f03b9e1dab5495fc9ebaf6b
gabrielly <gabriellycostadossantos063@gmail.com>
2026-08-06 19:58:20 -0300

    sdfsd

--- service/matriculas.js
+++ service/matriculas.js
@@ -1,5 +1,9 @@
-let matriculas = []
-let id = 1
+let matriculas = [
+  { id: 1, nome: "João Silva", curso: "Musculação" },
+  { id: 2, nome: "Maria Souza", curso: "Crossfit" },
+  { id: 3, nome: "Pedro Lima", curso: "Natação" }
+]
+let id = 4
 
 function matricular(nome, curso) {
   const aluno = { id: id++, nome, curso }
